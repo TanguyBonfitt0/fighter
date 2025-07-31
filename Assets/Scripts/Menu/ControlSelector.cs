60a9962dba16adb121c5daa1f94708c58c31a232
TanguyBonfitt0 <tanguy.bonfitto@supinfo.com>
2025-07-31 16:08:33 +0200

    debut choix manette clavier

---
+++ Assets/Scripts/Menu/ControlSelector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlSelector : MonoBehaviour
+{
+    public void ChoisirClavier()
+    {
+        PlayerPrefs.SetString("controlType", "keyboard");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ChoisirManette()
+    {
+        PlayerPrefs.SetString("controlType", "gamepad");
+        SceneManager.LoadScene("Game");
+    }
+}
